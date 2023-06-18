d60af35d1e1ef13928f43c3e3d7dc9c43a902a2b
albvnovs <albvnovs@gmail.com>
2023-06-19 00:56:52 +0600

    new ch

--- drone/src/components/header.jsx
+++ drone/src/components/header.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import { React, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import { BiChat } from "react-icons/bi";
 
 export default function Header() {
+  const [burger, setBurger] = useState(false);
+
+  const handleBurgerClick = () => {
+    setBurger(!burger);
+  };
   return (
     <div>
-      <header className="lg:flex hidden justify-between text-white items-center lg:pl-20 lg:pr-20 w-full h-[90px] text-sm">
-        <div className="text-white">LOGO</div>
-        <nav className="w-[500px] flex justify-between">
+      <div className="text-white lg:hidden fixed p-1 rounded-sm right-5 top-5 z-20">
+        {burger ? (
+          <AiOutlineClose
+            onClick={handleBurgerClick}
+            className="text-[#FE390C]"
+            size={25}
+          />
+        ) : (
+          <GiHamburgerMenu
+            onClick={handleBurgerClick}
+            className="text-[#FE390C]"
+            size={25}
+          />
+        )}
+      </div>
+      <a>
+        <BiChat className="fixed bottom-5 left-5 text-[#FE390C]" size={30} />
+      </a>
+      <header
+        className={
+          burger
+            ? "transition-all z-10 flex flex-col justify-around items-center bg-black/95 fixed right-0 w-[270px] h-screen"
+            : "lg:flex hidden justify-between text-white items-center lg:pl-20 lg:pr-20 w-full h-[90px] text-sm"
+        }
+      >
+        <div className={burger ? "fixed top-5 left-[140px]" : "text-white"}>
+          LOGO
+        </div>
+        <nav
+          className={
+            burger ? "flex flex-col text-xl" : "w-[500px] flex justify-between"
+          }
+        >
           <NavLink to="/" className="hover:text-[#FE390C] transition-all">
             главная
           </NavLink>
--- drone/src/page/about.jsx
+++ drone/src/page/about.jsx
@@ -3,43 +3,37 @@ import drones from "../img/home/drone3.png";
 import mulitfac from "../img/about/mulitfac.png";
 import desic from "../img/about/desic.jpg";
 import opyl from "../img/about/opyl.jpg";
-
-import droneOne from "../img/home/droneOne.png";
-import droneTwo from "../img/home/drone-two.png";
-import droneThree from "../img/home/droneThree.png";
 import Header from "../components/header";
 
 const About = () => {
   return (
     <div className="mx-auto">
-      <div className="bg-site w-full h-96 min-h-full ">
+      <div className="bg-site w-full h-screen lg:h-96">
         <Header />
-        <div className="flex justify-center items-center flex-col  pt-20 ">
-          <img className="w-2/5" src={drones} alt="" />
-          <div className="absolute">
-            <h1 className="text-white text-6xl text font-bold">
-              СЕЛЬСКОЕ ХОЗЯЙСТВО
-            </h1>
-          </div>
+        <div className="flex justify-center items-center flex-col  pt-20">
+          <img className="lg:w-2/5 w-90" src={drones} alt="" />
         </div>
       </div>
       <section className="">
-        <div className="w-full flex justify-around items-center">
+        <h1 className="text-center lg:pt-[100px] pb-10 lg:text-4xl font-bold">
+          Сельское хозяйство
+        </h1>
+        <div className="w-full flex lg:justify-around flex-col lg:flex-row items-center">
           <div>
             <img
-              className="w-96 my-20 border border-[#FE390C]"
+              className="lg:w-96 w-60 border border-[#FE390C]"
               src={mulitfac}
               alt=""
             />
           </div>
           <div>
-            <div className="flex items-center">
+            <div className="flex justify-center lg:justify-start items-center">
               <h1 className="font-bold text-6xl text-[#FE390C]">01</h1>
-              <h3 className="w-96 font-bold text-xl text-black">
+              <h3 className="lg:w-96 w-40 font-bold text-sm lg:text-xl text-black">
                 МУЛЬТИСПЕКТРАЛЬНОЕ ИССЛЕДОВАНИЕ
               </h3>
             </div>
-            <p className="text-sm w-[600px]">
+            <p className="text-sm lg:w-[600px] w-[330px] lg:text-left text-center">
               Наша компания предлагает инновационное решение для фермеров —
               Мультиспектральную съемку полей дроном. Мы предлагаем новейшие
               технологии в области сельского хозяйства с применением агродронов,
@@ -49,13 +43,15 @@ const About = () => {
             </p>
           </div>
         </div>
-        <div className="w-full flex justify-around items-center">
+        <div className="w-full flex lg:justify-around flex-col-reverse lg:flex-row items-center">
           <div>
-            <div className="flex items-center">
+            <div className="flex justify-center lg:justify-start items-center">
               <h1 className="font-bold text-6xl text-[#FE390C]">02</h1>
-              <h3 className="w-96 font-bold text-xl text-black">ДЕСИКАЦИЯ</h3>
+              <h3 className="lg:w-96 w-40 font-bold text-sm lg:text-xl text-black">
+                ДЕСИКАЦИЯ
+              </h3>
             </div>
-            <p className="text-sm w-[600px]">
+            <p className="text-sm lg:w-[600px] w-[330px] lg:text-left text-center">
               Наша компания предлагает инновационное решение для фермеров —
               Мультиспектральную съемку полей дроном. Мы предлагаем новейшие
               технологии в области сельского хозяйства с применением агродронов,
@@ -66,26 +62,28 @@ const About = () => {
           </div>
           <div>
             <img
-              className="w-96 my-20 border border-[#FE390C]"
+              className="lg:w-96 w-60 border border-[#FE390C]"
               src={desic}
               alt=""
             />
           </div>
         </div>
-        <div className="w-full flex justify-around items-center">
+        <div className="w-full flex lg:justify-around flex-col lg:flex-row items-center">
           <div>
             <img
-              className="w-96 my-20 border border-[#FE390C]"
+              className="lg:w-96 w-60 border border-[#FE390C]"
               src={opyl}
               alt=""
             />
           </div>
           <div>
-            <div className="flex items-center">
+            <div className="flex justify-center lg:justify-start items-center">
               <h1 className="font-bold text-6xl text-[#FE390C]">03</h1>
-              <h3 className="w-96 font-bold text-xl text-black">ОПЫЛЕНИЕ</h3>
+              <h3 className="lg:w-96 w-40 font-bold text-sm lg:text-xl text-black">
+                ОПЫЛЕНИЕ
+              </h3>
             </div>
-            <p className="text-sm w-[600px]">
+            <p className="text-sm lg:w-[600px] w-[330px] lg:text-left text-center">
               Наша компания предлагает инновационное решение для фермеров —
               Мультиспектральную съемку полей дроном. Мы предлагаем новейшие
               технологии в области сельского хозяйства с применением агродронов,
@@ -93,50 +91,6 @@ const About = () => {
               помочь сельскохозяйственным производителям поднять качество и
               количество урожая.
             </p>
-          </div>
-        </div>
-      </section>
-      <section className="text-gray-700 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap justify-around text-center">
-            <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
-              <div className="border-2 border-gray-600 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-                <img src={droneOne} alt="" />
-                <h2 className="title-font font-bold text-3xl mb-4 text-gray-900">
-                  ОПЫЛЕНИЕ
-                </h2>
-                <p className="leading-relaxed">
-                  В штатной комплектации наши дроны предназначены для опыления
-                  жидких ядохимикатов и удобрений.
-                </p>
-              </div>
-            </div>
-            <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
-              <div className="border-2 border-gray-600 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-                <img src={droneTwo} alt="" />
-                <h2 className="title-font  mb-2 font-bold text-2xl text-gray-900">
-                  ДЕСИКАЦИЯ
-                </h2>
-                <p className="leading-relaxed">
-                  При установлении опциональной системы, мы способны
-                  разбрасывать семена, гранулированные удобрения и кормовые
-                  продукты.
-                </p>
-              </div>
-            </div>
-            <div className="p-4 md:w-1/3 sm:w-1/2 w-full">
-              <div className="border-2 border-gray-600 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-                <img src={droneThree} alt="" />
-                <h2 className="title-font font-bold fnt-medium text-1xl text-gray-900">
-                  МУЛЬТИСПЕКТРАЛЬНАЯ КАМЕРА
-                </h2>
-                <p className="leading-relaxed">
-                  Благодаря тепловизионной камере мы можем определить слабые
-                  места почвы. Также можем определить и состоянию созревания
-                  урожая.
-                </p>
-              </div>
-            </div>
           </div>
         </div>
       </section>
